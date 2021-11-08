e73a7090cf5cb4070655f50d49efce895a8ced90
Jahid Hassan <32175937+Jahid1499@users.noreply.github.com>
2021-11-08 17:10:13 +0600

    slider show done

--- src/RestApi/AppUrl.js
+++ src/RestApi/AppUrl.js
@@ -1,9 +1,12 @@
 
 class AppUrl{
-    static BaseUrl = 'http://10.10.10.101/VisaCovidCenterDevelopment/public/api/';
-    static UserCreate = this.BaseUrl+'user/create';
-    static UserLogin = this.BaseUrl+'user/login';
-    static OtpCheck = this.BaseUrl+'user/otpCheck';
+    static BaseUrl = 'http://10.10.10.101/VisaCovidCenterDevelopment/public/';
+    static UserCreate = this.BaseUrl+'api/user/create';
+    static UserLogin = this.BaseUrl+'api/user/login';
+    static OtpCheck = this.BaseUrl+'api/user/otpCheck';
+
+
+    static Slider = this.BaseUrl+'api/home/slider';
 }
 
 export default AppUrl;
